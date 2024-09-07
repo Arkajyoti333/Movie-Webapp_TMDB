e9f8c0f16bd14008ceabf8476830699f22f613b3
Soumyajit Ray <soumyajitray2002@gmail.com>
2024-09-08 00:21:21 +0530

    moviecard edited

--- src/Components/HomeScreen/homeScreen.js
+++ src/Components/HomeScreen/homeScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-
+import './homeScreen.css'
 import FetchPopularMovies from '../../TMDB_Services/tmdbServices';
 import MovieCard from '../MovieCard/MovieCard';
 
@@ -18,7 +18,7 @@ const HomeScreen = () => {
 
   return (
     <>
-      <div className="App container mx-auto px-4 mt-40">
+      <div className="App container bg-black mx-auto px-4 ">
       <h1 className="text-4xl font-bold text-center my-8">Popular Movies</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {movies.map((movie) => (
--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,19 +1,19 @@
 import React from 'react';
-
+import './Moviecard.css'
 const MovieCard = ({ movie }) => {
   const { title, poster_path, overview, vote_average, release_date } = movie;
   const posterUrl = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
   return (
-    <div className="movie-card max-w-sm rounded overflow-hidden shadow-lg m-4">
-      <img className="w-full" src={posterUrl} alt={title} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-gray-700 text-base">{overview.substring(0, 100)}...</p>
+    <div className="movie-card">
+      <img className="movieImage" src={posterUrl} alt={title} />
+      <div className="movieDesc">
+        <div className="movieTitle">{title}</div>
+        <p className="movieOvrview">{overview.substring(0, 100)}...</p>
       </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Rating: {vote_average}</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Release Date: {release_date}</span>
+      <div className="movieDetails">
+        <span className="rating">Rating: {vote_average}</span>
+        <span className="releaseDate">Release Date: {release_date}</span>
       </div>
     </div>
   );
