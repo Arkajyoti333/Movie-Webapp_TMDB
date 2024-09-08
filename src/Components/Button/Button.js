3df8606fb56c31bd215d35cde98f0bce6b101279
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-09-08 22:35:00 +0530

    update movie card ...

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,16 @@
+
+
+
+
+const Button = () => {
+  return (
+    <>
+      <button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+        Watch Now
+      </button>
+    </>
+  );
+};
+
+
+export default Button;
--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import './Moviecard.css'
+
 const MovieCard = ({ movie }) => {
   const { title, poster_path, overview, vote_average, release_date } = movie;
   const posterUrl = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
-  return (
-    <div className="movie-card">
-      <img className="movieImage" src={posterUrl} alt={title} />
-      <div className="movieDesc">
-        <div className="movieTitle">{title}</div>
-        <p className="movieOvrview">{overview.substring(0, 100)}...</p>
-      </div>
-      <div className="movieDetails">
-        <span className="rating">Rating: {vote_average}</span>
-        <span className="releaseDate">Release Date: {release_date}</span>
-      </div>
-    </div>
+
+
+
+   return (  
+   
+   <>
+   <div className="movie-card">
+  <img className="movieImage" src={posterUrl} alt={title} />
+  <div className="movieDesc">
+    <div className="movieTitle">{title}</div>
+    <p className="movieOvrview">{overview.substring(0, 100)}...</p>
+  </div>
+  <div className="movieDetails">
+    <span className="rating">⭐ {vote_average}</span>
+    <span className="releaseDate">{release_date}</span>
+  </div>
+</div>
+
+   </>
   );
+
+
+       
+  
 };
 
 export default MovieCard;
+// return (
+
+  //   // <div className="movie-card">
+  //   //   <img className="movieImage" src={posterUrl} alt={title} />
+  //   //   <div className="movieDesc">
+  //   //     <div className="movieTitle">{title}</div>
+  //   //     <p className="movieOvrview">{overview.substring(0, 100)}...</p>
+  //   //   </div>
+  //   //   <div className="movieDetails">
+  //   //     <span className="rating">Rating: {vote_average}</span>
+  //   //     <span className="releaseDate">Release Date: {release_date}</span>
+  //   //   </div>
+  //   // </div>
+  // );
